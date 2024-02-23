17ebc55f151a04285f1864b4162c6db66c8f7721
Nikhil Ranjan Kumar <nikhil.kumar@braincells.in>
2024-02-23 23:09:32 +0530

    [hotfix] : added route get method

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 import express from "express";
 import "./db/init.js";
 import dotenv from "dotenv";
-import cors from "cors"
-import userRoutes from "./routes/user-routes.js"
+import cors from "cors";
+import userRoutes from "./routes/user-routes.js";
 dotenv.config();
-
 
 const App = express();
 App.use(express.urlencoded({ extended: false }));
 App.use(express.json());
 App.use(cors());
 
+App.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Hello World",
+  });
+});
 
-App.use("/v1/api", userRoutes)
+App.use("/v1/api", userRoutes);
 
+App.use("*", (req, res) => {
+  return res.status(500).json({
+    statusCode: 500,
+    success: false,
+    message: "Route not found",
+    data: null,
+  });
+});
 
-App.use("*", (req ,res) => {
-    return res.status(500).json({
-        statusCode : 500,
-        success : false,
-        message : "Route not found",
-        data : null
-    })
-})
+App.use((err, req, res, next) => {
+  return res.status(err.statusCode || 500).json({
+    statusCode: err.statusCode || 500,
+    success: false,
+    message: err.message,
+    data: null,
+  });
+});
 
-
-App.use((err , req , res , next) => {
-    return res.status(err.statusCode || 500).json({
-        statusCode: err.statusCode || 500,
-        success: false,
-        message: err.message,
-        data: null
-    })
-})
-
-
-const PORT = process.env.PORT
-App.listen(PORT , () => {
-    console.log(`Server Started at port ${PORT}`);
-})
+const PORT = process.env.PORT;
+App.listen(PORT, () => {
+  console.log(`Server Started at port ${PORT}`);
+});
